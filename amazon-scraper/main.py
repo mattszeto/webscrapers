900ff3a33c4db07f055ad39d5d0a56c13d774ef6
mattszeto <ms8288@gmail.com>
2020-09-06 12:22:34 -0700

    simple amazon product scraper template

---
+++ amazon-scraper/main.py
@@ -0,0 +1,49 @@
+import requests
+from bs4 import BeautifulSoup
+import smtplib
+
+# amazon product URL
+URL = 'AMAZON URL'
+# google User-Agent in your browser and paste here
+user_agent = 'USER AGENT'
+# target price
+target_price = 200
+
+
+headers = {
+    "User-Agent": user_agent}
+
+page = requests.get(URL, headers=headers)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+price = soup.find(id="priceblock_ourprice").get_text()
+converted_price = price[:-3]
+
+# price target
+if(converted_price < target_price):
+    send_email()
+
+# need to enable 2-step verification and enable app passwords
+
+
+def send_email():
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+# REPLACE USERNAME AND GENERATED PASSWORD HERE
+    server.login('USERNAME', 'GENERATED PASSWORD')
+
+    subject = 'Price has changed'
+    body = 'Check the amazon link: ' + URL
+    msg = f"Subject: {subject}\n\n{body}"
+
+# REPLACE SENDER EMAIL AND RECEIVER EMAIL
+    server.sendmail(
+        'SENDER EMAIL',
+
+        'RECEIVER EMAIL',
+        msg
+    )
+    print('Email sent')
+    server.quit()
